24fa2410ccc4304104a7085639b519348f054b4a
Sergey Bacho <nicedfx@gmail.com>
2017-07-04 21:32:23 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,24 +1,33 @@
-package org.library;
-
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchOrderByException {
 
 
         Book first = new Book(1493945L, "Fill S.", "Audit", new Date());
         Book second = new Book(38213L, "Sidd R.", "More", new Date());
         Book third = new Book(2358L, "Siery A.", "Authentication", new Date());
 
+
         InternetLibrary library = new InternetLibrary();
         library.addBook(first);
         library.addBook(second);
         library.addBook(third);
-        System.out.println("Unsorted: " + "\n"  + library.toString() );
 
-        System.out.println("Sorted Library: " + "\n" + library.sort().toString() );
+        File file = new File("new");
+        file.createNewFile();
+        try (FileOutputStream out = new FileOutputStream(file))
+            for(Book book: library.ge)
+//        System.out.println("Unsorted: " + "\n"  + library.toString() );
+//
+//        System.out.println("Sorted Library: " + "\n" + library.sort().toString() );
+//
+//        System.out.println(library.findBook(new Date()));
 
 //        System.out.println(library.findBook("Morsae"));
 //        System.out.println(library.findBookByAuthor("Sidd R."));
@@ -57,6 +66,9 @@ public class Application {
 //        }
 //        System.out.println("\n");
 //
-   }
+
+        System.out.println(library.sort(OrderBy.BY_AUTHOR));
+
+    }
 
 }
